70cf415f273be2defa9a2ef501fbe365dd0d174e
王茂己 <wangmaoji@wangmaojideMacBook-Pro.local>
2021-04-15 23:04:52 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -10,6 +9,27 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  setup () {
+    const classmates = {
+      rock: {
+        age: 23,
+        color: 'blue',
+      },
+      alysa: {
+        age: 23,
+        color: 'green',
+      }
+    }
+    console.log(classmates?.john ?? "no that guy")
+
+    if(!classmates.john) {
+      console.log('no that guy')
+    }
+
+    return {
+      classmates,
+    }
   }
 }
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,118 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang="pug">
+Swiper(
+    :slides-per-view="1"
+    :space-between="50"
+    navigation
+    :pagination="{ clickable: true }"
+    :scrollbar="{ draggable: true }"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  )
+  swiper-slide(v-if="routerState == 'first'") slide 1
+  swiper-slide(v-if="routerState == 'second'") slide 2
+  swiper-slide(v-if="routerState == 'third'") slide 3
+
+progress(id="file" max="100" value="50" @click="historyGo({foo: 'first'})") 
+br
+progress(id="file" max="100" value="60" @click="historyGo({foo: 'second'})")
+br
+progress(id="file" max="100" value="70" @click="historyGo({foo: 'third'})") 
+//- img(src="https://www.itread01.com/uploads/images/20170305/1488674316-5250.jpg")
+
 </template>
 
+
+
 <script>
+
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+
+import 'swiper/swiper-bundle.css';
+
+// Import Swiper styles
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+import { ref } from 'vue'
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+
+    function onSwiper() {
+      // console.log()
+    }
+    function onSlideChange() {
+      console.log('slide change')
+      // console.log(window.history.back())
+    }
+    console.log(window)
+    console.log(window.performance.timing.requestStart)
+
+    let routerState = ref(null)
+
+    function historyGo (stateObj) {
+      let { foo }= stateObj
+      console.log(foo)
+      history.pushState(stateObj, "", `#${foo}`)
+      console.log(`history: `, history)
+      console.log(`state:`, history.state)
+      routerState.value = history.state.foo
+      console.log(routerState.value)
+      // let url = window.URL(URL.pathname)
+      console.log('url', location.hash)
+    }
+
+   
+    
+    return {
+      routerState,
+      onSwiper,
+      onSlideChange,
+      historyGo,
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style scoped lang="scss">
+.swiper-container {
+  width: 600px;
+  height: 300px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+.swiper-slide {
+    width: 100%;
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+
+    /* Center slide text vertically */
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center;
+  }
 </style>
